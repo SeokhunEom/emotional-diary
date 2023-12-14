aa3d61c964388d23589bcd6ac7d745adc1ceb93d
tommya98 <tommya98@naver.com>
2023-12-14 17:36:28 +0900

    STYLE: DiaryItem 스타일 변경

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,14 +1,15 @@
 const DiaryItem = ({ author, content, created_date, emotion, id }) => {
   return (
     <div className="DiaryItem">
-      <div className="info"></div>
-      <span>
-        작성자 : {author} | 감정점수 : {emotion}
-      </span>
-      <br />
-      <span className="date">
-        {new Date(created_date).toLocaleDateString()}
-      </span>
+      <div className="info">
+        <span>
+          | 작성자 : {author} | 감정점수 : {emotion} |
+        </span>
+        <br />
+        <span className="date">
+          {new Date(created_date).toLocaleDateString()}
+        </span>
+      </div>
       <div className="content">{content}</div>
     </div>
   );
