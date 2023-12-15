9ee323a509b9ac3c27afec99172bc8fbbd54a9e6
tommya98 <tommya98@naver.com>
2023-12-15 11:52:41 +0900

    FEAT: 일기 delete 구현

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,11 @@
-const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+const DiaryItem = ({
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+  onDelete,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -11,6 +18,15 @@ const DiaryItem = ({ author, content, created_date, emotion, id }) => {
         </span>
       </div>
       <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          if (window.confirm("일기를 정말 삭제하시겠습니까?")) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
