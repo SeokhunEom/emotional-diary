059a771b3065359806b495acac76fa308411f863
tommya98 <tommya98@naver.com>
2023-12-17 10:47:39 +0900

    FEAT: React.memo 최적화 연습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -67,6 +68,7 @@ function App() {
 
   return (
     <div className="App">
+      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
